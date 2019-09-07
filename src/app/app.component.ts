999c4f53e0505628da1192827623e6e1f7c613e4
William Dorneles <williamdorneles@protonmail.com>
2019-09-07 18:53:44 -0300

    Injecting document on ssr aplication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { isNgContent } from '@angular/compiler';
+import { inject } from '@angular/core/testing';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'site-umporcento';
+
+  constructor(@Inject(DOCUMENT) private document, ) {
+
+  }
+
+  ngOnInit() {
+    console.log(this.document.URL);
+  }
 }
