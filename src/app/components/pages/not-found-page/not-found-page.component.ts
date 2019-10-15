3ed583b62be488074b0344860bc20f7a022f0b6d
William Dornels <williamdorneles@protonmail.com>
2019-10-14 22:46:06 -0300

    404 page

--- src/app/components/pages/not-found-page/not-found-page.component.ts
+++ src/app/components/pages/not-found-page/not-found-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { StateInitialiserService } from 'src/app/services/state-initialiser.service';
+import { StateService } from 'src/app/services/state.service';
 
 @Component({
   selector: 'app-not-found-page',
@@ -6,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./not-found-page.component.scss']
 })
 export class NotFoundPageComponent implements OnInit {
-
-  constructor() { }
+  state: StateService;
+  constructor() { 
+    this.state = new StateInitialiserService().getState();
+  }
 
   ngOnInit() {
   }
