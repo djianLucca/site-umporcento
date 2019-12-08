00ea0d5a22dcb08a762df93edff17d70b98bda99
William Dornels <williamdorneles@protonmail.com>
2019-12-07 21:05:41 -0300

    Adjusting details and responsivity

--- src/app/services/enums/socialiconurlenum.ts
+++ src/app/services/enums/socialiconurlenum.ts
@@ -1,6 +1,6 @@
 export enum SocialIconUrl {
     Facebook = 'https://www.facebook.com/umporcento.cc',
     Instagram = 'https://www.instagram.com/umporcento.cc/',
-    Spotify = 'https://vimeo.com/umporcentocc',
-    Email = 'contato@umporcento.cc'
+    Spotify = 'https://link.spotify',
+    Email = 'mailto:contato@umporcento.cc'
 }
